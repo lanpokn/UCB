234b0a6357423acb3361ba8d33f9979d45a0f807
lanpokn <2606629917@qq.com>
2022-01-17 20:21:39 +0800

    2,worse than qlearning

--- main.py
+++ main.py
@@ -1,68 +1,3 @@
-"""
-Reinforcement learning maze example.
-Red rectangle:          explorer.
-Black rectangles:       hells       [reward = -1].
-Yellow bin circle:      paradise    [reward = +1].
-All other states:       ground      [reward = 0].
-This script is the main part which controls the update method of this example.
-The RL is in RL_brain.py.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-"""
-
-# from my_model import Maze
-# from Qlearn import QLearningTable
-
-# def learn_init(actions):
-#     for action in actions:
-#         self.learn(s)
-#     pass
-
-# # qlearn update
-# def update():
-#     for episode in range(100):
-#         # initial observation
-#         observation = env.reset()
-
-#         while True:
-#             # fresh env
-#             env.render()
-
-#             # RL choose action based on observation
-#             action = RL.choose_action(str(observation))
-
-#             # RL take action and get next observation and reward
-#             observation_, reward, done = env.step(action)
-
-#             # RL learn from this transition
-#             RL.learn(str(observation), action, reward, str(observation_))
-
-#             # swap observation
-#             observation = observation_
-
-#             # break while loop when end of this episode
-#             if done:
-#                 break
-
-#     # end of game
-#     print('game over')
-#     env.destroy()
-    
-
-# if __name__ == "__main__":
-#     env = Maze()
-#     RL = QLearningTable(actions=list(range(env.n_actions)))
-
-#     env.after(100, update)
-#     env.mainloop()
-
-
-
-
-
-
-
-
-
 from my_model import Maze
 from UCB import UCBTable
 
@@ -110,10 +45,11 @@ def update():
     env.destroy()
 
 #ucb learn
+#UCB WILL FOREVER CHANGED TO THESE UNEXplored ,because of the increase of t
 if __name__ == "__main__":
     env = Maze()
     RL = UCBTable(actions=list(range(env.n_actions)))
     learn_init(RL)
 
-    env.after(100, update)
+    env.after(500, update)
     env.mainloop()
