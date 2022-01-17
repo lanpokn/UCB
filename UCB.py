91bd2c7f41741af5c5180b33edfaf84803640c7b
lanpokn <2606629917@qq.com>
2022-01-17 19:55:10 +0800

    first,done

--- UCB.py
+++ UCB.py
@@ -1,49 +1,75 @@
 import numpy as np
 import pandas as pd
+from math import *
 import scipy.signal
 from gym.spaces import Box, Discrete
+
 
 # I may need a table to memory each Gaossian score,mean,variance?
 #from mofan education
 
+#UCB need time to be choosed and total
+#first all bandit should be chosen to avoid dividing zero
+
 class UCBTable:
     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
         self.actions = actions  # a list
-        self.lr = learning_rate
-        self.gamma = reward_decay
-        self.epsilon = e_greedy
+        self.T = 1
+        self.actions_table = {}
+        for action in actions:
+            self.actions_table.update({action:[0,0]})#  [chosen times,total score]
+        # self.lr = learning_rate
+        # self.gamma = reward_decay
+        # self.epsilon = e_greedy
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
-
+    
     def choose_action(self, observation):
         self.check_state_exist(observation)
         # action selection
-        if np.random.uniform() < self.epsilon:
-            # choose best action
-            state_action = self.q_table.loc[observation, :]
-            # some actions may have the same value, randomly choose on in these actions
-            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
-        else:
-            # choose random action
-            action = np.random.choice(self.actions)
-        return action
+        # if np.random.uniform() < self.epsilon:
+        #     # choose best action
+        #     state_action = self.q_table.loc[observation, :]
+        #     # some actions may have the same value, randomly choose on in these actions
+        #     action = np.random.choice(state_action[state_action == np.max(state_action)].index)
+        # else:
+        #     # choose random action
+        #     action = np.random.choice(self.actions)
+        
+        #choose best UCB
+        max_index = 0
+        max_UCB = 0
+        index = 0 
+        for action in self.actions:
+            temp_score = self.UCB(action)
+            if(temp_score>max_UCB):
+                max_UCB = temp_score
+                max_index = index
+            index+=1
+        self.T +=1
+        print(max_UCB)
+        return self.actions[max_index]
 
-    def learn(self, s, a, r, s_):
-        self.check_state_exist(s_)
-        q_predict = self.q_table.loc[s, a]
-        if s_ != 'terminal':
-            q_target = r + self.gamma * self.q_table.loc[s_, :].max()  # next state is not terminal
-        else:
-            q_target = r  # next state is terminal
-        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
+    def learn(self, state, action, reward, state_next,init = False):
+        self.check_state_exist(state_next)
+        times_past,totalreward_past = self.actions_table[action][0],self.actions_table[action][1]
+        self.actions_table.update({action:[times_past+1,totalreward_past+reward]})
+        
 
     def check_state_exist(self, state):
-        if state not in self.q_table.index:
-            # append new state to q table
-            self.q_table = self.q_table.append(
-                pd.Series(
-                    [0]*len(self.actions),
-                    index=self.q_table.columns,
-                    name=state,
-                )
-            )
+        #TODO
+        # if state not in self.q_table.index:
+        #     # append new state to q table
+        #     self.q_table = self.q_table.append(
+        #         pd.Series(
+        #             [0]*len(self.actions),
+        #             index=self.q_table.columns,
+        #             name=state,
+        #         )
+        #     )
+        pass
+    
+    def UCB(self,a):
+        UCB_score = self.actions_table[a][1]+sqrt(2*log2(self.T)/self.actions_table[a][0])
+        return UCB_score
+        
             
--- main.py
+++ main.py
@@ -9,10 +9,77 @@ The RL is in RL_brain.py.
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 """
 
+# from my_model import Maze
+# from Qlearn import QLearningTable
+
+# def learn_init(actions):
+#     for action in actions:
+#         self.learn(s)
+#     pass
+
+# # qlearn update
+# def update():
+#     for episode in range(100):
+#         # initial observation
+#         observation = env.reset()
+
+#         while True:
+#             # fresh env
+#             env.render()
+
+#             # RL choose action based on observation
+#             action = RL.choose_action(str(observation))
+
+#             # RL take action and get next observation and reward
+#             observation_, reward, done = env.step(action)
+
+#             # RL learn from this transition
+#             RL.learn(str(observation), action, reward, str(observation_))
+
+#             # swap observation
+#             observation = observation_
+
+#             # break while loop when end of this episode
+#             if done:
+#                 break
+
+#     # end of game
+#     print('game over')
+#     env.destroy()
+    
+
+# if __name__ == "__main__":
+#     env = Maze()
+#     RL = QLearningTable(actions=list(range(env.n_actions)))
+
+#     env.after(100, update)
+#     env.mainloop()
+
+
+
+
+
+
+
+
+
 from my_model import Maze
-from Qlearn import QLearningTable
+from UCB import UCBTable
 
+#before main loop of UCB, each bandit should be chosen for once, and do not increase T in this time
+def learn_init(RL):
+    for action in RL.actions:
+        # initial observation each time
+        observation = env.reset()
+        env.render()
+        observation_, reward, done = env.step(action)
 
+        # RL learn from this transition
+        RL.learn(str(observation), action, reward, str(observation_),init = True)
+
+        # swap observation, in tradition MAB, they are the same
+        observation = observation_
+    pass
 def update():
     for episode in range(100):
         # initial observation
@@ -42,9 +109,11 @@ def update():
     print('game over')
     env.destroy()
 
+#ucb learn
 if __name__ == "__main__":
     env = Maze()
-    RL = QLearningTable(actions=list(range(env.n_actions)))
+    RL = UCBTable(actions=list(range(env.n_actions)))
+    learn_init(RL)
 
     env.after(100, update)
     env.mainloop()
